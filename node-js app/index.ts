4bda113e755b2a56264dcd0bb61c08892cd1817c
Giansimone Coccia <S1097845@studenti.univpm.it>
2024-07-03 17:40:21 +0200

    Update index.ts

--- node-js app/index.ts
+++ node-js app/index.ts
@@ -1,28 +1,12 @@
-/**import { exec } from 'child_process';
-
-// Definisci il nome del file di seed per la tabella contents
-const seedFile = '20240702133741-seed-contents.js';
-
-// Definisci il comando sequelize-cli da eseguire
-const command = `npx sequelize-cli db:migrate`;
-
-// Esegui il comando utilizzando exec di Node.js
-exec(command, (error, stdout, stderr) => {
-  if (error) {
-    console.error(`Errore durante l'esecuzione del comando: ${error.message}`);
-    return;
-  }
-  if (stderr) {
-    console.error(`Errore: ${stderr}`);
-    return;
-  }
-  console.log(`Output del comando: ${stdout}`);
-});**/
-
-
 import express from 'express';
+import routes from './src/routes/routes';  // Assicurati che il percorso sia corretto
 
 const app = express();
+
+app.use(express.json());
+
+// Utilizza le rotte definite in routes/routes.ts
+app.use('/api', routes);
 
 app.get('/', (req, res) => {
     console.log('Received request on /');
@@ -32,19 +16,3 @@ app.get('/', (req, res) => {
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`App listening on http://localhost:${port}`));
-
-/*import express from 'express';
-import routes from '../src/routes/routes';
-
-const app = express();
-
-// Middleware per gestire il parsing del body delle richieste
-app.use(express.json());
-
-// Utilizzo delle rotte definite nel router
-app.use('/', routes);
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => console.log(`App listening on http://localhost:${port}`));*/
-
