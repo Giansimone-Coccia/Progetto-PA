e741515355f3c2eda2bc3afc76f6f9febe434526
Walter Di Sabatino <91206724+Walter-Di-Sabatino@users.noreply.github.com>
2024-07-03 10:42:54 +0200

    Update

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { exec } from 'child_process';
+
+// Definisci il nome del file di seed per la tabella contents
+const seedFile = '20240702133741-seed-contents.js';
+
+// Definisci il comando sequelize-cli da eseguire
+const command = `npx sequelize-cli db:seed --seed ${seedFile}`;
+
+// Esegui il comando utilizzando exec di Node.js
+exec(command, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Errore durante l'esecuzione del comando: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.error(`Errore: ${stderr}`);
+    return;
+  }
+  console.log(`Output del comando: ${stdout}`);
+});
