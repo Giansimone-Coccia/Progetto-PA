9318c19abf8b3d1f8e7d2570b7d377e8ab2583ec
Walter Di Sabatino <91206724+Walter-Di-Sabatino@users.noreply.github.com>
2024-07-03 14:15:26 +0200

    Modifiche

--- Dockerfile
+++ Dockerfile
@@ -7,5 +7,10 @@ RUN npm install
 
 COPY . .
 
+RUN npm run build
+
+ENV PORT=3000
+
 EXPOSE 3000
-CMD ["npm", "run", "start"]
+
+CMD ["npm", "start"]
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { exec } from 'child_process';
+/**import { exec } from 'child_process';
 
 // Definisci il nome del file di seed per la tabella contents
 const seedFile = '20240702133741-seed-contents.js';
@@ -17,4 +17,34 @@ exec(command, (error, stdout, stderr) => {
     return;
   }
   console.log(`Output del comando: ${stdout}`);
+});**/
+
+
+/*import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    console.log('Received request on /');
+    res.json({ message: 'Docker is easy 👍' });
 });
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`App listening on http://localhost:${port}`));*/
+
+import express from 'express';
+import routes from '../src/routes/routes';
+
+const app = express();
+
+// Middleware per gestire il parsing del body delle richieste
+app.use(express.json());
+
+// Utilizzo delle rotte definite nel router
+app.use('/', routes);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`App listening on http://localhost:${port}`));
+
