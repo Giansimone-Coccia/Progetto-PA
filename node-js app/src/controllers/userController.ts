26dc9fd51bdfa5ae7b1a26e4fa89d23087a9b65c
Giansimone Coccia <S1097845@studenti.univpm.it>
2024-07-08 11:37:13 +0200

    Credit recharge

--- node-js app/src/controllers/userController.ts
+++ node-js app/src/controllers/userController.ts
@@ -82,7 +82,49 @@ class UserController {
       res.status(500).json({ message: 'Internal server error' });
     }
   };
+
+  public creditRecharge = async (req: CustomRequest, res: Response) => {
+    const emailUser = req.body.emailUser;
+    const tokenUser = req.body.tokenUser;
+    console.log(`email: ${emailUser}`);
+    console.log(`token: ${tokenUser}`);
+
+    if (!emailUser) {
+      return res.status(400).json({ message: 'Email is required' });
+    }
   
+    if (tokenUser == null) { // Check for both null and undefined
+      return res.status(400).json({ message: 'Token value is required' });
+    }
+  
+    try {
+      const user = await this.userService.findUserByEmail(emailUser);
+  
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+  
+      const userId = user.id;
+      console.log(`UserID: ${userId}`);
+      if (!Number.isInteger(userId)) {
+        return res.status(500).json({ message: 'Invalid user ID' });
+      }
+
+      const newTokensValue = user.tokens + Number(tokenUser);
+  
+      const updateSuccessful = await this.userService.updateUser(userId, { tokens: newTokensValue });
+  
+      if (updateSuccessful) {
+        return res.status(200).json({ message: `Operation completed. New token value: ${newTokensValue}` });
+      } else {
+        return res.status(500).json({ message: 'Failed to update user tokens' });
+      }
+    } catch (error) {
+      console.error(`Error updating user tokens: ${error}`);
+      return res.status(500).json({ message: 'Internal server error' });
+    }
+  };
+
 }
 
 export default UserController;
